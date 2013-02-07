95c8812c9a68b84b56ce5b3f96a226b7ab42f702
TOMITA Keisuke <keisuke@node3-vps.host1free.com>
2013-02-07 18:19:11 +0900

    Add migemo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -241,6 +241,15 @@ C-uをつけるとウィンドウを閉じる。"
 ;; minibuf-isearch
 (require 'minibuf-isearch)
 
+;; migemo
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(load-library "migemo")
+
 ;; org
 (require 'org)
 (setq org-activate-links '(date bracket radio tag date footnote angle))
